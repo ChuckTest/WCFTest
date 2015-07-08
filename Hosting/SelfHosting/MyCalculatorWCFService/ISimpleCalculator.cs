d6b150845aab5800d0fa9ef8f55d756c6eefb00c
ChuckLu <244657538@qq.com>
2015-07-08 14:33:34 +0800

    1.增加了WCF中Hosting的selfHosting代码，以控制台托管为例

---
+++ Hosting/SelfHosting/MyCalculatorWCFService/ISimpleCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace MyCalculatorWCFService
+{
+    [ServiceContract]
+    public interface ISimpleCalculator
+    {
+        [OperationContract]
+        int Sum(int number1, int number2);
+    }
+}
---
+++ Hosting/SelfHosting/MyCalculatorWCFServiceHost/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using MyCalculatorWCFService;
+
+namespace MyCalculatorWCFServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a URI to serve as the base address
+            Uri httpUrl = new Uri("http://localhost:8090/MyCalculatorWCFService/SimpleCalculator");
+
+            //Create ServiceHost
+            ServiceHost host = new ServiceHost(typeof(MyCalculatorService), httpUrl);
+
+            //Add a service endpoint
+            host.AddServiceEndpoint(typeof(ISimpleCalculator), new WSHttpBinding(), "");
+
+            //Enable metadata exchange
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            host.Description.Behaviors.Add(smb);
+
+            //Start the Service
+            host.Open();
+            Console.WriteLine("Service is host at " + DateTime.Now.ToString());
+            Console.WriteLine("Host is running... Press  key to stop");
+            Console.ReadLine();
+        }
+    }
+}
