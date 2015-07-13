e32c1ed555219c739e8d90dc5c46755dc8e5b983
ChuckLu <244657538@qq.com>
2015-07-13 18:32:59 +0800

    1.添加Yun解决方案中的客户端调用服务引用的代码

---
+++ Yun/YunClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YunClient.YunServiceReference;
+using System.Data;
+
+namespace YunClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServiceClient client = new ServiceClient();
+
+                DataTable dataTable = client.SelectProjectByUser("Zbm007");
+                int row = dataTable.Rows.Count;
+                int column = dataTable.Columns.Count;
+                for (int i = 0; i < column; i++)
+                {
+                    Console.Write(string.Format("{0} ", dataTable.Columns[i]));
+                }
+                Console.WriteLine();
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                    {
+                        Console.Write(string.Format("{0} ", dataTable.Rows[i][j]));
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
