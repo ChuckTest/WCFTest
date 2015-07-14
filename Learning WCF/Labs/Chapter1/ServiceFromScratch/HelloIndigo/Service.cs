5c13e3524166cb30d677558819b0867663643e66
ChuckLu <244657538@qq.com>
2015-07-14 14:12:04 +0800

    1.添加Learning WCF书中的ServiceFromScratch解决方案中的第一个类库项目

---
+++ Learning WCF/Labs/Chapter1/ServiceFromScratch/HelloIndigo/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace HelloIndigo
+{
+    [ServiceContract(Name = "http://www.thatindigogirl.com/samples/2006/06")]
+    public interface IHelloIndigoService
+    {
+        [OperationContract]
+        string HelloIndigo();
+    }
+
+    public class HelloIndigoService : IHelloIndigoService
+    {
+        public string HelloIndigo()
+        {
+            return "Hello Indigo";
+        }
+    }
+}
