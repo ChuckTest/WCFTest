2de660d06eef32d6b9676ed9bc798f68561022a4
ChuckLu <244657538@qq.com>
2015-07-14 17:37:50 +0800

    1.通过添加一个WCF服务，以及对app.config的右键-->编辑WCF配置，通过配置来控制Host

--- Learning WCF/Labs/Chapter1/ServiceFromScratch/Host/Program.cs
+++ Learning WCF/Labs/Chapter1/ServiceFromScratch/Host/Program.cs
@@ -10,11 +10,17 @@ namespace Host
     {
         static void Main(string[] args)
         {
-            using (ServiceHost host = new ServiceHost(typeof(HelloIndigo.HelloIndigoService), new Uri("http://localhost:8000/HelloIndigo")))
+            //using (ServiceHost host = new ServiceHost(typeof(HelloIndigo.HelloIndigoService), new Uri("http://localhost:8000/HelloIndigo")))
+            //{
+            //    host.AddServiceEndpoint(typeof(HelloIndigo.IHelloIndigoService), new BasicHttpBinding(), "HelloIndigoService");
+            //    host.Open();
+            //    Console.WriteLine("Press <ENTER> to terminate the service host");
+            //    Console.ReadLine();
+            //}
+            using (ServiceHost host = new ServiceHost(typeof(HelloIndigoService)))
             {
-                host.AddServiceEndpoint(typeof(HelloIndigo.IHelloIndigoService), new BasicHttpBinding(), "HelloIndigoService");
                 host.Open();
-                Console.WriteLine("Press <ENTER> to terminate the service host");
+                Console.WriteLine("Press <ENTER> to terminate the host application");
                 Console.ReadLine();
             }
         }
