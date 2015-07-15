1973d5be188f5b4d2101b842adf5bb5668ebcc8e
ChuckLu <244657538@qq.com>
2015-07-15 16:30:00 +0800

    1.提交网站托管服务的初始代码

---
+++ Learning WCF/Labs/Chapter1/IISHostedService/Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press <ENTER> to terminate Client.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learning WCF/Labs/Chapter1/IISHostedService/HelloIndigo/HelloIndigoService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ServiceModel;
+
+namespace HelloIndigo
+{
+    [ServiceContract(Namespace = "http://www.thatindigogirl.com/samples/2006/06")]
+    public interface IHelloIndigoService
+    {
+        [OperationContract]
+        string HelloIndigo();
+    }
+
+    public class HelloIndigoService : IHelloIndigoService
+    {
+        public string HelloIndigo()
+        {
+            return "Hello Indigo";
+        }
+    }
+}
