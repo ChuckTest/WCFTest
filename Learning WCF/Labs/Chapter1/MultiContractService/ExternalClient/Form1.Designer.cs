7e768991788b9ed993ef1358c3e8959fe0d98135
ChuckLu <244657538@qq.com>
2015-07-17 17:12:09 +0800

    Consuming external services with a generated proxy 1.通过添加服务引用的方式调用服务，VS会自动生成代理 2.在External项目中添加调用代理的代码

--- Learning WCF/Labs/Chapter1/MultiContractService/ExternalClient/Form1.Designer.cs
+++ Learning WCF/Labs/Chapter1/MultiContractService/ExternalClient/Form1.Designer.cs
@@ -28,57 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonOperation1 = new System.Windows.Forms.Button();
+            this.buttonOperation2 = new System.Windows.Forms.Button();
+            this.buttonOperation3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonOperation1
             // 
-            this.button1.Location = new System.Drawing.Point(98, 74);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Operation1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonOperation1.Location = new System.Drawing.Point(98, 74);
+            this.buttonOperation1.Name = "buttonOperation1";
+            this.buttonOperation1.Size = new System.Drawing.Size(75, 23);
+            this.buttonOperation1.TabIndex = 0;
+            this.buttonOperation1.Text = "Operation1";
+            this.buttonOperation1.UseVisualStyleBackColor = true;
+            this.buttonOperation1.Click += new System.EventHandler(this.buttonOperation1_Click);
             // 
-            // button2
+            // buttonOperation2
             // 
-            this.button2.Location = new System.Drawing.Point(98, 104);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Operation2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonOperation2.Location = new System.Drawing.Point(98, 104);
+            this.buttonOperation2.Name = "buttonOperation2";
+            this.buttonOperation2.Size = new System.Drawing.Size(75, 23);
+            this.buttonOperation2.TabIndex = 1;
+            this.buttonOperation2.Text = "Operation2";
+            this.buttonOperation2.UseVisualStyleBackColor = true;
+            this.buttonOperation2.Click += new System.EventHandler(this.buttonOperation2_Click);
             // 
-            // button3
+            // buttonOperation3
             // 
-            this.button3.Location = new System.Drawing.Point(98, 134);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Operation3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonOperation3.Location = new System.Drawing.Point(98, 134);
+            this.buttonOperation3.Name = "buttonOperation3";
+            this.buttonOperation3.Size = new System.Drawing.Size(75, 23);
+            this.buttonOperation3.TabIndex = 2;
+            this.buttonOperation3.Text = "Operation3";
+            this.buttonOperation3.UseVisualStyleBackColor = true;
+            this.buttonOperation3.Click += new System.EventHandler(this.buttonOperation3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonOperation3);
+            this.Controls.Add(this.buttonOperation2);
+            this.Controls.Add(this.buttonOperation1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonOperation1;
+        private System.Windows.Forms.Button buttonOperation2;
+        private System.Windows.Forms.Button buttonOperation3;
     }
 }
 
--- Learning WCF/Labs/Chapter1/MultiContractService/ExternalClient/Form1.cs
+++ Learning WCF/Labs/Chapter1/MultiContractService/ExternalClient/Form1.cs
@@ -5,14 +5,70 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using ExternalClient.ServiceA;
+using ExternalClient.ServiceB;
+using System.ServiceModel;
 
 namespace ExternalClient
 {
     public partial class Form1 : Form
     {
+        ServiceAClient proxyA;
+        ServiceBClient proxyB;
+        string message = string.Empty;
+
         public Form1()
         {
             InitializeComponent();
+            InitProxy();
+        }
+
+        private void InitProxy()
+        {
+            //传递的参数是endpoint的名字
+            proxyA = new ServiceAClient("BasicHttpBinding_IServiceA");
+            proxyB = new ServiceBClient("BasicHttpBinding_IServiceB");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //ServiceAClient和ServiceBClient继承自ClientBase<TChannel>类
+            CloseProxy(proxyA as ICommunicationObject);
+            CloseProxy(proxyB as ICommunicationObject);
+        }
+
+        private void CloseProxy(ICommunicationObject proxy)
+        {
+            if (proxy == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (proxy.State == CommunicationState.Faulted)
+            {
+                proxy.Abort();
+            }
+            else
+            {
+                proxy.Close();
+            }
+        }
+
+        private void buttonOperation1_Click(object sender, EventArgs e)
+        {
+            message = proxyA.Operation1();
+            MessageBox.Show(message);
+        }
+
+        private void buttonOperation2_Click(object sender, EventArgs e)
+        {
+            message = proxyA.Operation2();
+            MessageBox.Show(message);
+        }
+
+        private void buttonOperation3_Click(object sender, EventArgs e)
+        {
+            message = proxyB.Operation3();
+            MessageBox.Show(message);
         }
     }
 }
