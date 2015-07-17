1859474df9fd88c3f8fbf70f61e99e22eb209092
ChuckLu <244657538@qq.com>
2015-07-17 18:39:07 +0800

    1.增加章节二中，服务契约中，所需要的Host项目以及BusinessServices项目

---
+++ Learning WCF/Labs/Chapter2/ServiceContracts_Part1/BusinessServices/IServiceA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ServiceModel;
+
+namespace BusinessServices
+{
+    [ServiceContract]
+    public interface IServiceA
+    {
+        [OperationContract]
+        string Operation1();
+        [OperationContract]
+        string Operation2();
+    }
+}
---
+++ Learning WCF/Labs/Chapter2/ServiceContracts_Part1/Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ServiceModel;
+using BusinessServices;
+using System.ServiceModel.Description;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(ServiceA));
+            host.Open();
+
+            ServiceEndpointCollection endpoints = host.Description.Endpoints;
+            foreach (ServiceEndpoint item in endpoints)
+            {
+                Console.WriteLine(item.Address);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press <ENTER> to terminate Host");
+            Console.ReadLine();
+            host.Close();
+
+        }
+    }
+}
