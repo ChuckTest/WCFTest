20c24899cb43c372c8b4d0b26f330c0d3ce2b13e
ChuckLu <244657538@qq.com>
2015-07-20 20:15:46 +0800

    1.增加契约继承的初始代码

---
+++ Learning WCF/Labs/Chapter2/ServiceContractVersioning_Inheritance/Host/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessServices;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(ServiceA));
+            host.Open();
+            ServiceEndpointCollection endpoints = host.Description.Endpoints;
+            foreach (ServiceEndpoint item in endpoints)
+            {
+                Console.WriteLine(item.Address);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Please press Enter to terminate the Host");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
---
+++ Learning WCF/Labs/Chapter2/ServiceContractVersioning_Inheritance/WinClientV1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WinClientV1.localhost;
+
+namespace WinClientV1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (ServiceAContractClient proxy = new ServiceAContractClient())
+            {
+                string s = proxy.Operation1();
+                MessageBox.Show(s);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (ServiceAContractClient proxy = new ServiceAContractClient())
+            {
+                string s = proxy.Operation2();
+                MessageBox.Show(s);
+            }
+        }
+    }
+}
