b40ca6f814d4c5dca70fad6a64b494e66a38afb3
ChuckLu <244657538@qq.com>
2015-07-20 10:58:41 +0800

    1.提交ServiceContracts中的客户端初始代码

---
+++ Learning WCF/Labs/Chapter2/ServiceContracts_Part1/WinClient/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WinClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOperation1 = new System.Windows.Forms.Button();
+            this.buttonOperation2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOperation1
+            // 
+            this.buttonOperation1.Location = new System.Drawing.Point(79, 80);
+            this.buttonOperation1.Name = "buttonOperation1";
+            this.buttonOperation1.Size = new System.Drawing.Size(75, 23);
+            this.buttonOperation1.TabIndex = 0;
+            this.buttonOperation1.Text = "Operation1";
+            this.buttonOperation1.UseVisualStyleBackColor = true;
+            this.buttonOperation1.Click += new System.EventHandler(this.buttonOperation1_Click);
+            // 
+            // buttonOperation2
+            // 
+            this.buttonOperation2.Location = new System.Drawing.Point(79, 122);
+            this.buttonOperation2.Name = "buttonOperation2";
+            this.buttonOperation2.Size = new System.Drawing.Size(75, 23);
+            this.buttonOperation2.TabIndex = 1;
+            this.buttonOperation2.Text = "Operation2";
+            this.buttonOperation2.UseVisualStyleBackColor = true;
+            this.buttonOperation2.Click += new System.EventHandler(this.buttonOperation2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonOperation2);
+            this.Controls.Add(this.buttonOperation1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOperation1;
+        private System.Windows.Forms.Button buttonOperation2;
+    }
+}
+
---
+++ Learning WCF/Labs/Chapter2/ServiceContracts_Part1/WinClient/Service References/localhost.cs
@@ -0,0 +1,61 @@
+﻿namespace WinClient.localhost
+{
+
+
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "3.0.0.0")]
+    [System.ServiceModel.ServiceContractAttribute(ConfigurationName = "WinClient.localhost.IServiceA")]
+    public interface IServiceA
+    {
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IServiceA/Operation1", ReplyAction = "http://tempuri.org/IServiceA/Operation1Response")]
+        string Operation1();
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IServiceA/Operation2", ReplyAction = "http://tempuri.org/IServiceA/Operation2Response")]
+        string Operation2();
+    }
+
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "3.0.0.0")]
+    public interface IServiceAChannel : WinClient.localhost.IServiceA, System.ServiceModel.IClientChannel
+    {
+    }
+
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "3.0.0.0")]
+    public partial class ServiceAClient : System.ServiceModel.ClientBase<WinClient.localhost.IServiceA>, WinClient.localhost.IServiceA
+    {
+
+        public ServiceAClient()
+        {
+        }
+
+        public ServiceAClient(string endpointConfigurationName) :
+            base(endpointConfigurationName)
+        {
+        }
+
+        public ServiceAClient(string endpointConfigurationName, string remoteAddress) :
+            base(endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public ServiceAClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) :
+            base(endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public ServiceAClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) :
+            base(binding, remoteAddress)
+        {
+        }
+
+        public string Operation1()
+        {
+            return base.Channel.Operation1();
+        }
+
+        public string Operation2()
+        {
+            return base.Channel.Operation2();
+        }
+    }
+}
