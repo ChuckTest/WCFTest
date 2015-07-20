6e258a7052651dd50684f8480445370581f55263
ChuckLu <244657538@qq.com>
2015-07-20 14:07:54 +0800

    增加ServiceContracts_Part1的副本，并命名为ServiceContracts_Part2

---
+++ Learning WCF/Labs/Chapter2/ServiceContracts_Part2/WinClient/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WinClient.localhost;
+
+namespace WinClient
+{
+    public partial class Form1 : Form
+    {
+        ServiceAClient proxy = new ServiceAClient();
+        string message = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOperation1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                message = proxy.Operation1();
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonOperation2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                message = proxy.Operation2();
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (proxy != null)
+            {
+                proxy.Close();
+            }
+        }
+    }
+}
---
+++ Learning WCF/Labs/Chapter2/ServiceContracts_Part2/WinClient/Service References/localhost.cs
@@ -0,0 +1,61 @@
+﻿namespace WinClient.localhost
+{
+
+
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "3.0.0.0")]
+    [System.ServiceModel.ServiceContractAttribute(Name = "ServiceAContract", Namespace = "http://www.thatindigogirl.com/samples/2006/06", ConfigurationName = "WinClient.localhost.IServiceA")]
+    public interface IServiceA
+    {
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://www.thatindigogirl.com/samples/2006/06/ServiceAContract/Operation1", ReplyAction = "http://www.thatindigogirl.com/samples/2006/06/ServiceAContract/Operation1Response")]
+        string Operation1();
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://www.thatindigogirl.com/samples/2006/06/ServiceAContract/Operation2", ReplyAction = "http://www.thatindigogirl.com/samples/2006/06/ServiceAContract/Operation2Response")]
+        string Operation2();
+    }
+
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "3.0.0.0")]
+    public interface IServiceAChannel : WinClient.localhost.IServiceA, System.ServiceModel.IClientChannel
+    {
+    }
+
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "3.0.0.0")]
+    public partial class ServiceAClient : System.ServiceModel.ClientBase<WinClient.localhost.IServiceA>, WinClient.localhost.IServiceA
+    {
+
+        public ServiceAClient()
+        {
+        }
+
+        public ServiceAClient(string endpointConfigurationName) :
+            base(endpointConfigurationName)
+        {
+        }
+
+        public ServiceAClient(string endpointConfigurationName, string remoteAddress) :
+            base(endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public ServiceAClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) :
+            base(endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public ServiceAClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) :
+            base(binding, remoteAddress)
+        {
+        }
+
+        public string Operation1()
+        {
+            return base.Channel.Operation1();
+        }
+
+        public string Operation2()
+        {
+            return base.Channel.Operation2();
+        }
+    }
+}
