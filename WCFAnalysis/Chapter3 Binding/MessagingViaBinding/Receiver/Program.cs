7bfed8b49817e1c7637582723e0b4eabe99da7e6
ChuckLu <244657538@qq.com>
2015-08-14 10:46:16 +0800

    1.直接通过Binding进行消息通信  《wcf全面解析》 3.1.2

---
+++ WCFAnalysis/Chapter3 Binding/MessagingViaBinding/Receiver/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Xml.Linq;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main()
+        {
+            Uri listenUri = new Uri("http://192.168.1.18/listener");
+            Binding binding = new BasicHttpBinding();
+            IChannelListener<IReplyChannel> channelListener = binding.BuildChannelListener<IReplyChannel>(listenUri);
+            channelListener.Open();
+
+            IReplyChannel replyChannel = channelListener.AcceptChannel(TimeSpan.MaxValue);
+            if(replyChannel!=null)
+            {
+                replyChannel.Open();
+                Console.WriteLine("Start Listening");
+                while (true)
+                {
+                    RequestContext requestContext = replyChannel.ReceiveRequest(TimeSpan.MaxValue);
+                    if (requestContext != null)
+                    {
+                        Console.WriteLine(requestContext.RequestMessage);
+                        requestContext.Reply(CreateReplyMessage(binding));
+                    }
+                }
+            }
+        }
+
+        private static Message CreateReplyMessage(Binding binding)
+        {
+            Message message;
+            string action = "http://www.chucklu.com/calculatorService/AddResponse";
+            XNamespace xNamespace = "http://www.chucklu.com";
+            XElement body =
+                new XElement(new XElement(xNamespace + "AddResponse", new XElement(xNamespace + "AddResult", 3)));
+            message = Message.CreateMessage(binding.MessageVersion, action, body);
+            return message;
+        }
+    }
+}
---
+++ WCFAnalysis/Chapter3 Binding/MessagingViaBinding/Sender/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Xml.Linq;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main()
+        {
+            Uri listenUri=new Uri("http://192.168.1.18/listener");
+            Binding binding = new BasicHttpBinding();
+
+            IChannelFactory<IRequestChannel> channelFactory = binding.BuildChannelFactory<IRequestChannel>();
+            channelFactory.Open();
+
+            IRequestChannel channel = channelFactory.CreateChannel(new EndpointAddress(listenUri));
+            channel.Open();
+
+            Message replyMessage = channel.Request(CreateRequestMessage(binding));
+            Console.WriteLine(replyMessage);
+
+            Console.WriteLine("Press Enter to terminate the client");
+            Console.ReadLine();
+        }
+
+        private static Message CreateRequestMessage(Binding binding)
+        {
+            Message message;
+            string action = "http://www.chucklu.com/calculatorService/Add";
+            XNamespace xNamespace = "http://www.chucklu.com";
+            XElement body =
+                new XElement(new XElement(xNamespace + "Add", new XElement(xNamespace + "x", 1),
+                    new XElement(xNamespace + "y", 2)));
+            message = Message.CreateMessage(binding.MessageVersion, action, body);
+            return message;
+        }
+    }
+}
