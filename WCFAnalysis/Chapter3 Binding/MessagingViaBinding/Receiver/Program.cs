4fbd21ff946a511e44542c3ec878e56fc104c8d4
ChuckLu <244657538@qq.com>
2015-08-17 10:53:30 +0800

    1.给wcf全面解析中的MessagingViaBinding添加注释

--- WCFAnalysis/Chapter3 Binding/MessagingViaBinding/Receiver/Program.cs
+++ WCFAnalysis/Chapter3 Binding/MessagingViaBinding/Receiver/Program.cs
@@ -11,34 +11,42 @@ namespace Receiver
         {
             Uri listenUri = new Uri("http://192.168.1.18/listener");
             Binding binding = new BasicHttpBinding();
+            //创建信道监听器
             IChannelListener<IReplyChannel> channelListener = binding.BuildChannelListener<IReplyChannel>(listenUri);
+            //打开信道监听器
             channelListener.Open();
 
+            //创建回复信道
             IReplyChannel replyChannel = channelListener.AcceptChannel(TimeSpan.MaxValue);
             if(replyChannel!=null)
             {
+                //打开回复信道
                 replyChannel.Open();
                 Console.WriteLine("Start Listening");
+                //开始监听
                 while (true)
                 {
+                    //接收请求消息
                     RequestContext requestContext = replyChannel.ReceiveRequest(TimeSpan.MaxValue);
                     if (requestContext != null)
                     {
+                        //输出请求消息
                         Console.WriteLine(requestContext.RequestMessage);
-                        requestContext.Reply(CreateReplyMessage(binding));
+                        //回复消息
+                        requestContext.Reply(CreateReplyMessage(binding.MessageVersion));
                     }
                 }
             }
         }
 
-        private static Message CreateReplyMessage(Binding binding)
+        private static Message CreateReplyMessage(MessageVersion messageVersion)
         {
             Message message;
             string action = "http://www.chucklu.com/calculatorService/AddResponse";
             XNamespace xNamespace = "http://www.chucklu.com";
             XElement body =
                 new XElement(new XElement(xNamespace + "AddResponse", new XElement(xNamespace + "AddResult", 3)));
-            message = Message.CreateMessage(binding.MessageVersion, action, body);
+            message = Message.CreateMessage(messageVersion, action, body);
             return message;
         }
     }
--- WCFAnalysis/Chapter3 Binding/MessagingViaBinding/Sender/Program.cs
+++ WCFAnalysis/Chapter3 Binding/MessagingViaBinding/Sender/Program.cs
@@ -12,20 +12,25 @@ namespace Sender
             Uri listenUri=new Uri("http://192.168.1.18/listener");
             Binding binding = new BasicHttpBinding();
 
+            //创建信道工厂
             IChannelFactory<IRequestChannel> channelFactory = binding.BuildChannelFactory<IRequestChannel>();
+            //打开信道工厂
             channelFactory.Open();
 
-            IRequestChannel channel = channelFactory.CreateChannel(new EndpointAddress(listenUri));
-            channel.Open();
+            //创建请求信道
+            IRequestChannel requestChannel = channelFactory.CreateChannel(new EndpointAddress(listenUri));
+            //打开请求信道
+            requestChannel.Open();
 
-            Message replyMessage = channel.Request(CreateRequestMessage(binding));
+            //发送请求消息，并接收回复消息
+            Message replyMessage = requestChannel.Request(CreateRequestMessage(binding.MessageVersion));
             Console.WriteLine(replyMessage);
 
             Console.WriteLine("Press Enter to terminate the client");
             Console.ReadLine();
         }
 
-        private static Message CreateRequestMessage(Binding binding)
+        private static Message CreateRequestMessage(MessageVersion messageVersion)
         {
             Message message;
             string action = "http://www.chucklu.com/calculatorService/Add";
@@ -33,7 +38,7 @@ namespace Sender
             XElement body =
                 new XElement(new XElement(xNamespace + "Add", new XElement(xNamespace + "x", 1),
                     new XElement(xNamespace + "y", 2)));
-            message = Message.CreateMessage(binding.MessageVersion, action, body);
+            message = Message.CreateMessage(messageVersion, action, body);
             return message;
         }
     }
